6237800713a0ff942fcd61c0f3592fa411485747
matan amzaleg <matanamz@gmail.com>
2023-02-22 14:12:08 +0200

    spotifly-added

--- src/services/project-service.js
+++ src/services/project-service.js
@@ -11,7 +11,7 @@ const projects = [
         id:"p101",
         title: "Portobello",
         description:"A pixel perfect Trello clone app",
-        technologies:"Vue.js, Socket IO, MongoDB",
+        technologies:"Vue.js, VueX, Socket IO, MongoDB, Sass, NodeJS, Express",
         link:"https://portobello-backend-production.up.railway.app/#/",
         expanding:"Portobello is a project management tool that allows users to create boards to organize tasks and projects. Each board has multiple lists and cards to represent tasks, ideas, etc. Users can add labels, comments, attachments, due dates, assign members, and create checklists within cards to track progress. Additionally, users can set up notifications for card activities such as comments or due date reminders. It also includes a new feature 'Smart Checklist' supported by OpenAI bot."
     },
@@ -19,33 +19,33 @@ const projects = [
         id:"p102",
         title: "Appsus",
         description:"Exclusive 3 in 1 app that contains Emailing system, Notes & Book store",
-        technologies:"Vue.js",
+        technologies:"Vue.js, VueX, CSS",
         link:"https://matanamzaleg.github.io/appSus/#/",
         expanding:"A 3-in-1 app that combines email, notes, and a bookstore. The app allows users to manage their email, create notes, and browse books within the same platform. The email feature includes standard functionality such as composing, replying, and turning email into notes. while the notes feature includes the ability to add images, videos, and files to notes. The bookstore feature allows users to browse and search for books, view book details and write reviews."
     },
     {
         id:"p103",
-        title: "Mister-burger",
-        description:"A restaurant alike app, that simulates menu and order selecting",
-        technologies:"Angular",
-        expanding:"A restaurant-like app that simulates menu and order selection allows users to view and order food from a digital menu. Users are able to add items to their cart, customize their orders and pay for their order directly through the app. Such apps are widely used by restaurants and food delivery services to provide customers with a convenient way to order food."
+        title: "Spotifly",
+        description:"A Spotify clone app",
+        technologies:"React, Redux, NodeJs, Express, MongoDB",
+        link:"https://spotifly-app.onrender.com/",
+        expanding:"Spotifly is a music streaming app , with a modern design and a large music library. Users can create personalized playlists, discover new music, and search for their favorite artists and songs. Additionally, Spotifly fetches the lyrics of the songs, making it easy for users to sing along to their favorite tunes. The app allows users to sync their music across all devices for easy access. Overall, Spotifly provides a comprehensive music experience for music lovers looking for a Spotify-like platform."
     },
     {
         id:"p104",
-        title: "Mister-bitcoin",
-        description:"A Bitcoin transaction app where you can log in and transfer BTC to your contacts",
-        technologies:"React",
-        link:"https://matanamzaleg.github.io/mister-bitcoin/#/signup",
-        expanding:"A Bitcoin transaction app allows users to transfer bitcoin. The app allow users to log in and transfer BTC to their contacts and also view the BTC states. Users are able to view their bitcoin balance, transaction history, and exchange rate. The app also have features such as add and search for contacts, and view the current state of the Bitcoin network and the exchange rate. It is a convenient way for people to manage their Bitcoin transactions and stay up to date with the latest developments in the Bitcoin market."
-
+        title: "Mister-burger",
+        description:"A restaurant alike app, that simulates menu and order selecting",
+        technologies:"Angular, Rxjs, TypeScript, Sass",
+        expanding:"A restaurant-like app that simulates menu and order selection allows users to view and order food from a digital menu. Users are able to add items to their cart, customize their orders and pay for their order directly through the app. Such apps are widely used by restaurants and food delivery services to provide customers with a convenient way to order food."
     },
     {
         id:"p105",
-        title: "My-Blackjack",
-        description:"A BlackJack game app",
-        technologies:"Vanilla web, JavaScript, Bootstrap",
-        link:"https://matanamzaleg.github.io/My-Black-Jack/",
-        expanding:"Blackjack game app is a digital version of the popular card game. It is designed to emulate the experience of playing the game in a casino setting. The rules and gameplay are similar to traditional blackjack, where the goal is to have a hand value of 21 or as close to 21 as possible without going over, or 'busting'. The app allows players to place bets and make decisions based on their cards, such as hitting, standing, or splitting."
+        title: "Mister-bitcoin",
+        description:"A Bitcoin transaction app where you can log in and transfer BTC to your contacts",
+        technologies:"React, Redux, Sass",
+        link:"https://matanamzaleg.github.io/mister-bitcoin/#/signup",
+        expanding:"A Bitcoin transaction app allows users to transfer bitcoin. The app allow users to log in and transfer BTC to their contacts and also view the BTC states. Users are able to view their bitcoin balance, transaction history, and exchange rate. The app also have features such as add and search for contacts, and view the current state of the Bitcoin network and the exchange rate. It is a convenient way for people to manage their Bitcoin transactions and stay up to date with the latest developments in the Bitcoin market."
+
     },
     {
         id:"p106",
@@ -62,6 +62,14 @@ const projects = [
         technologies:"Html, Css, JavaScript",
         link:"https://matanamzaleg.github.io/MineSweeper/",
         expanding:"Minesweeper is a classic puzzle game that can be played on a computer or mobile device. The game typically consists of a grid of squares, with some squares hiding 'mines' and others not. The player's goal is to clear the board by revealing the squares without mines, while avoiding the ones that contain mines. The app includes a variety of difficulty levels, such as beginner, intermediate, and expert, each with a different size and number of mines. The app also includes all kind of helps that may assist you at the time needed."
+    },
+    {
+        id:"p108",
+        title: "My-Blackjack",
+        description:"A BlackJack game app",
+        technologies:"Vanilla web, JavaScript, Bootstrap",
+        link:"https://matanamzaleg.github.io/My-Black-Jack/",
+        expanding:"Blackjack game app is a digital version of the popular card game. It is designed to emulate the experience of playing the game in a casino setting. The rules and gameplay are similar to traditional blackjack, where the goal is to have a hand value of 21 or as close to 21 as possible without going over, or 'busting'. The app allows players to place bets and make decisions based on their cards, such as hitting, standing, or splitting."
     },
 ]
 
